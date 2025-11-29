a94ebdf09b5ac4402674ee2d1836f078b72939f0
Shubhradeep007 <shubhradeepbose79@gmail.com>
2025-11-29 13:16:42 +0530

    feat: introduce EmptyBoards component to prompt users to create their first board with API integration.

--- app/dashboard/_components/empty-board.tsx
+++ app/dashboard/_components/empty-board.tsx
@@ -38,7 +38,7 @@ const EmptyBoards = () => {
   return (
     <>
       <div className="h-full flex flex-col items-center justify-center">
-        <Image src="" alt="Empty" height={140} width={140} />
+        <Image src="/logo.png" alt="Empty" height={140} width={140} />
 
         <h2 className="text-2xl font-semibold mt-6">Create your first board!</h2>
 
